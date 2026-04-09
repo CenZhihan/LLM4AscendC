0dc23e831239dc7d71315f39ea563ce3148232bf
liuxiang <liuxiang417@sjtu.edu.cn>
2026-04-09 20:03:31 +0800

    Stop tracking ascend_custom_opp dir

--- ascend_custom_opp/vendors/customize/bin/set_env.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export ASCEND_CUSTOM_OPP_PATH=/aistor/sjtu/hpc_stor01/home/liuxiang/LLM4NPU/LLM4AscendC/ascend_custom_opp/vendors/customize:${ASCEND_CUSTOM_OPP_PATH}
-export LD_LIBRARY_PATH=/aistor/sjtu/hpc_stor01/home/liuxiang/LLM4NPU/LLM4AscendC/ascend_custom_opp/vendors/customize/op_api/lib/:${LD_LIBRARY_PATH}
